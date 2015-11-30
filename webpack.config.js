c985c0869c73afceb9d19ba6d6b27eba3521f2b3
minamorl <minamorl@users.noreply.github.com>
2015-11-30 16:35:26 +0900

    Supress unuseful error message

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-path    = require("path")
-webpack = require("webpack")
+'use strict'
+
+let path    = require("path")
+let webpack = require("webpack")
 module.exports = {
     context: __dirname + "/app",
     entry: "./entry.coffee",
@@ -13,7 +15,10 @@ module.exports = {
             { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.(gif|png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
             { test: /\.sass$/, loader: "style!css!sass?indentedSyntax" },
-            { test: /\.js$/, exclude: [/node_modules/, '/bower_components/'], loader: "babel-loader"}
+            { test: /\.js$/, exclude: [
+              /node_modules/, 
+              /bower_components/
+            ], loader: "babel-loader"}
         ]
     },
     resolve: {
